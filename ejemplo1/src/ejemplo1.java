86d6e921c26e68a480ba38753676096c6258cb08
alero911 <33299458+alero911@users.noreply.github.com>
2017-11-01 18:47:41 -0500

    Update ejemplo1.java

--- ejemplo1/src/ejemplo1.java
+++ ejemplo1/src/ejemplo1.java
@@ -4,7 +4,7 @@ package ejemplo1;
 public class ejemplo1 {
 
     public static void main(String[] args) {
-        System.out.println("Linea 1");
+        System.out.println("Linea 12");
         System.out.println("Linea 2");
     }
     
